83a14d2138a16a62e2e75e076293842c6d5ab759
UniverseVG <72432752+UniverseVG@users.noreply.github.com>
2020-10-21 10:50:40 +0530

    Add files via upload

---
+++ sublime2.py
@@ -0,0 +1,9 @@
+n1=int(input('First number\n'))
+n2=int(input('Second number\n'))
+print('Before Swapping values are..\n')
+print('First number',n1,'Second number',n2)
+m=n1
+n1=n2
+n2=m
+print('After Swapping values are..\n')
+print('First number',n1,'Second number',n2)
