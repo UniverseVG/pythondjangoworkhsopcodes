59d226b006c18e235e40fc3a9cc782edf0d56d79
GM Varun <72432752+UniverseVG@users.noreply.github.com>
2020-10-22 11:24:17 +0530

    Add files via upload

---
+++ sublime5.py
@@ -0,0 +1,3 @@
+n=int(input('Enter the number to display the table\n'))
+for i in range(1,11):
+	print(n*i)
---
+++ sublime6.py
@@ -0,0 +1,9 @@
+print('Enter 10 number\n')
+i=0
+sum=0
+while i<10:
+	n=int(input())
+	sum=sum+n
+	i=i+1
+
+print("The Sum of 10 numbers is ",sum)
